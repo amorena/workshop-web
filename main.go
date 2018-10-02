46df8b546b2d4a630c8ffa212d13ba42d9f04b5f
faiq <faiqrazarizvi@gmail.com>
2018-10-02 16:11:20 -0700

    adds a logger

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
@@ -17,13 +20,54 @@ var (
 		})
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	requests.Inc()
-	w.Write([]byte("Hello, is it me you're looking for?"))
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "custom 404")
+	}
+}
+
+func serveIndex() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.URL.Path != "/" {
+			errorHandler(w, r, http.StatusNotFound)
+			return
+		}
+		requests.Inc()
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello, is it me you're looking for?"))
+	})
+}
+
+func logging(l *log.Logger) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			l.Println(r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
+			next.ServeHTTP(w, r)
+		})
+	}
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	http.Handle("/metrics", promhttp.Handler())
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
+	logger.Println("Server is starting...")
+
+	router := http.NewServeMux()
+	router.Handle("/", serveIndex())
+	router.Handle("/metrics", promhttp.Handler())
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      logging(logger)(router),
+		ErrorLog:     logger,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+
+	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Fatalf("Could not listen on 8080: %v\n", err)
+	}
+
 }
